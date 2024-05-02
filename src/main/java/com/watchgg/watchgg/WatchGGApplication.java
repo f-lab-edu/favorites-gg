93b672274582f4636030b8b8e44d5e05127e8cc7
jeonghoHub <tjsrsdqq4@gmail.com>
2024-05-02 22:35:15 +0900

    feat : init

---
+++ src/main/java/com/watchgg/watchgg/WatchGGApplication.java
@@ -0,0 +1,13 @@
+package com.watchgg.watchgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchGGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchGGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/watchgg/watchgg/WatchGGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchgg.watchgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchGGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
