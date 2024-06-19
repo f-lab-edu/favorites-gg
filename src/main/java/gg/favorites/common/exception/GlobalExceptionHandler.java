8faa76d6aaa38ee28467f22b83d52e7fd3fa90f9
usa_develop <tjsrsdqq4@gmail.com>
2024-06-19 20:00:44 +0900

    :recycle: [#3] 유효성 검증 예외 핸들러 수정

--- src/main/java/gg/favorites/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gg/favorites/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package gg.favorites.common.exception;
 
 
-import gg.favorites.common.suppot.error.ErrorCode;
 import gg.favorites.common.suppot.reponse.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Level;
+
+import static gg.favorites.common.suppot.error.ErrorCode.COMMON_INVALID_PARAMETER;
+import static gg.favorites.common.suppot.error.ErrorCode.COMMON_SYSTEM_ERROR;
 
 @Slf4j
 @RestControllerAdvice
@@ -27,33 +33,22 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ApiResponse.error(e.getMessage()), e.getStatus());
     }
 
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
         log.error("Exception : {}", e.getMessage(), e);
-        return new ResponseEntity<>(ApiResponse.error(ErrorCode.COMMON_SYSTEM_ERROR.getErrorMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(ApiResponse.error(COMMON_SYSTEM_ERROR.getErrorMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
-
-//    /**
-//     * http status: 400 AND result: FAIL
-//     * request parameter 에러
-//     *
-//     * @param e
-//     * @return
-//     */
-////    @ResponseStatus(HttpStatus.BAD_REQUEST)
-////    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-////    public CommonResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
-////        log.warn("[BaseException] errorMsg = {}", NestedExceptionUtils.getMostSpecificCause(e).getMessage());
-////        BindingResult bindingResult = e.getBindingResult();
-////        FieldError fe = bindingResult.getFieldError();
-////        if (fe != null) {
-////            String message = "Request Error" + " " + fe.getField() + "=" + fe.getRejectedValue() + " (" + fe.getDefaultMessage() + ")";
-////            return CommonResponse.fail(message, ErrorCode.COMMON_INVALID_PARAMETER.name());
-////        } else {
-////            return CommonResponse.fail("요청한 값이 올바르지 않습니다.", ErrorCode.COMMON_INVALID_PARAMETER.name());
-////        }
-////    }
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiResponse<?>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("ValidException : {}", NestedExceptionUtils.getMostSpecificCause(e).getMessage());
+        BindingResult bindingResult = e.getBindingResult();
+        FieldError fe = bindingResult.getFieldError();
+        if (fe != null) {
+            String message = String.format("Request Error %s=%s (%s)", fe.getField(), fe.getRejectedValue(), fe.getDefaultMessage());
+            return new ResponseEntity<>(ApiResponse.error(message), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(ApiResponse.error(COMMON_INVALID_PARAMETER.getErrorMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
