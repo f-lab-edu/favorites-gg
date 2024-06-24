d876f576ac645cc3f155fba79654de50207f6071
jeonghoHub <tjsrsdqq4@gmail.com>
2024-06-24 23:31:27 +0900

    :recycle: [#3] Level 클래스 변경 및 시스템예외와 파라미터예외 ErrorCode의 Httpstatus 사용하게끔 수정

--- src/main/java/gg/favorites/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gg/favorites/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package gg.favorites.common.exception;
 
 import gg.favorites.common.suppot.reponse.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.logging.Level;
 
 import static gg.favorites.common.suppot.error.ErrorCode.COMMON_INVALID_PARAMETER;
 import static gg.favorites.common.suppot.error.ErrorCode.COMMON_SYSTEM_ERROR;
@@ -23,20 +22,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse<?>> handleBaseException(BaseException e) {
         Level logLevel = e.getLogLevel();
-        if (logLevel.equals(Level.SEVERE)) {
+        if (e.getLogLevel().equals(Level.ERROR)) {
             log.error("BaseException : {}", e.getMessage(), e);
-        } else if (logLevel.equals(Level.WARNING)) {
+        } else if (logLevel.equals(Level.WARN)) {
             log.warn("BaseException : {}", e.getMessage(), e);
         } else {
             log.info("BaseException : {}", e.getMessage(), e);
         }
-        return new ResponseEntity<>(ApiResponse.error(e.getMessage()), e.getStatus());
+        return new ResponseEntity<>(ApiResponse.error(e.getMessage()), e.getHttpStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
         log.error("Exception : {}", e.getMessage(), e);
-        return new ResponseEntity<>(ApiResponse.error(COMMON_SYSTEM_ERROR.getErrorMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(ApiResponse.error(COMMON_SYSTEM_ERROR.getErrorMessage()), COMMON_SYSTEM_ERROR.getHttpStatus());
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
@@ -48,7 +47,7 @@ public class GlobalExceptionHandler {
             String message = String.format("Request Error %s=%s (%s)", fe.getField(), fe.getRejectedValue(), fe.getDefaultMessage());
             return new ResponseEntity<>(ApiResponse.error(message), HttpStatus.BAD_REQUEST);
         } else {
-            return new ResponseEntity<>(ApiResponse.error(COMMON_INVALID_PARAMETER.getErrorMessage()), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(ApiResponse.error(COMMON_INVALID_PARAMETER.getErrorMessage()), COMMON_INVALID_PARAMETER.getHttpStatus());
         }
     }
 }
